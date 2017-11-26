08469e19c8c3613e776dda91adb2cba5f43f7a4e
J9 <jeffbaltazar09@gmail.com>
2017-11-26 09:19:52 +0800

    developing update(11/26/17)

--- www/js/index.js
+++ www/js/index.js
@@ -16,63 +16,64 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-ReactDOM.render(<center>
+ReactDOM.render(
+	<center>
 	<div class="line1"> Select Your Fighter</div></center>, document.getElementById('t'));
 
 let dom =<center> 
-	<table cellpadding="30%">
-		<tr>
-			<td><a href="1.html"><img src="img/1.jpg" value="1"/></a></td>
-			<td><a href="2.html"><img src="img/2.jpg" value="2"/></a></td>
-			<td><a href="3.html"><img src="img/3.jpg" value="3"/></a></td>
-			<td><a href="4.html"><img src="img/4.jpg" value="4"/></a></td>
-		</tr>
-		<tr>
-			<td><a href="5.html"><img src="img/5.png" value="5"/></a></td>
-			<td><a href="6.html"><img src="img/6.jpg" value="6"/></a></td>
-			<td><a href="7.html"><img src="img/7.jpg" value="7"/></a></td>
-			<td><a href="8.html"><img src="img/8.jpg" value="8"/></a></td>
-		</tr>
-		<tr>
-			<td><a href="9.html"><img src="img/9.png" /></a></td>
-			<td><a href="10.html"><img src="img/10.png" /></a></td>
-			<td><a href="11.html"><img src="img/11.png" /></a></td>
-			<td><a href="12.html"><img src="img/12.png" /></a></td>
-		</tr>
-		<tr>
-			<td><a href="13.html"><img src="img/13.png" /></a></td>
-			<td><a href="14.html"><img src="img/14.png" /></a></td>
-			<td><a href="15.html"><img src="img/15.jpg" class="lazy lazy-fadeIn"/></a></td>
-			<td><a href="16.html"><img src="img/16.jpg" class="lazy lazy-fadeIn"/></a></td>
-		</tr>
-		<tr>
-			<td><a href="17.html"><img src="img/17.png" class="lazy lazy-fadeIn"/></a></td>
-			<td><a href="18.html"><img src="img/18.jpg" class="lazy lazy-fadeIn"/></a></td>
-			<td><a href="19.html"><img src="img/19.jpg" class="lazy lazy-fadeIn"/></a></td>
-			<td><a href="20.html"><img src="img/20.jpg" class="lazy lazy-fadeIn"/></a></td>
-		</tr>
-		<tr>
-			<td><a href="21.html"><img src="img/21.png" class="lazy lazy-fadeIn"/></a></td>
-			<td><a href="22.html"><img src="img/22.jpg" class="lazy lazy-fadeIn"/></a></td>
-			<td><a href="23.html"><img src="img/23.jpg" class="lazy lazy-fadeIn"/></a></td>
-			<td><a href="24.html"><img src="img/24.png" class="lazy lazy-fadeIn"/></a></td>
-		</tr>
-		<tr>
-			<td><a href="25.html"><img src="img/25.jpg" class="lazy lazy-fadeIn"/></a></td>
-			<td><a href="26.html"><img src="img/26.jpg" class="lazy lazy-fadeIn"/></a></td>
-			<td><a href="27.html"><img src="img/27.jpg" class="lazy lazy-fadeIn"/></a></td>
-			<td><a href="28.html"><img src="img/28.jpg" class="lazy lazy-fadeIn"/></a></td>
-		</tr>
-		<tr>
-			<td><a href="29.html"><img src="img/29.jpg" class="lazy lazy-fadeIn"/></a></td>
-			<td><a href="30.html"><img src="img/30.jpg" class="lazy lazy-fadeIn"/></a></td>
-			<td><a href="31.html"><img src="img/31.jpg" class="lazy lazy-fadeIn"/></a></td>
-			<td><a href="32.html"><img src="img/32.jpg" class="lazy lazy-fadeIn"/></a></td>
-		</tr>
-		<tr>
-			<td><a href="33.html"><img src="img/33.jpg" class="lazy lazy-fadeIn"/></a></td>
-		</tr>
-	</table>
+	<div class="content-block">
+		<div class="row">
+			<div class="col-auto"><a href="1.html"><img src="img/1.jpg" value="1"/></a></div>
+			<div class="col-auto"><a href="2.html"><img src="img/2.jpg" value="2"/></a></div>
+			<div class="col-auto"><a href="3.html"><img src="img/3.jpg" value="3"/></a></div>
+			<div class="col-auto"><a href="4.html"><img src="img/4.jpg" value="4"/></a></div>
+		</div><br/>
+		<div class="row">
+			<div class="col-auto"><a href="5.html"><img src="img/5.png" value="5"/></a></div>
+			<div class="col-auto"><a href="6.html"><img src="img/6.jpg" value="6"/></a></div>
+			<div class="col-auto"><a href="7.html"><img src="img/7.jpg" value="7"/></a></div>
+			<div class="col-auto"><a href="8.html"><img src="img/8.jpg" value="8"/></a></div>
+		</div><br/>
+		<div class="row">
+			<div class="col-auto"><a href="9.html"><img src="img/9.png" /></a></div>
+			<div class="col-auto"><a href="10.html"><img src="img/10.png" /></a></div>
+			<div class="col-auto"><a href="11.html"><img src="img/11.png" /></a></div>
+			<div class="col-auto"><a href="12.html"><img src="img/12.png" /></a></div>
+		</div><br/>
+		<div class="row">
+			<div class="col-auto"><a href="13.html"><img src="img/13.png" /></a></div>
+			<div class="col-auto"><a href="14.html"><img src="img/14.png" /></a></div>
+			<div class="col-auto"><a href="15.html"><img src="img/15.jpg" /></a></div>
+			<div class="col-auto"><a href="16.html"><img src="img/16.jpg" /></a></div>
+		</div><br/>
+		<div class="row">
+			<div class="col-auto"><a href="17.html"><img src="img/17.png" class="lazy lazy-fadeIn"/></a></div>
+			<div class="col-auto"><a href="18.html"><img src="img/18.jpg" class="lazy lazy-fadeIn"/></a></div>
+			<div class="col-auto"><a href="19.html"><img src="img/19.jpg" class="lazy lazy-fadeIn"/></a></div>
+			<div class="col-auto"><a href="20.html"><img src="img/20.jpg" class="lazy lazy-fadeIn"/></a></div>
+		</div><br/>
+		<div class="row">
+			<div class="col-auto"><a href="21.html"><img src="img/21.png" class="lazy lazy-fadeIn"/></a></div>
+			<div class="col-auto"><a href="22.html"><img src="img/22.jpg" class="lazy lazy-fadeIn"/></a></div>
+			<div class="col-auto"><a href="23.html"><img src="img/23.jpg" class="lazy lazy-fadeIn"/></a></div>
+			<div class="col-auto"><a href="24.html"><img src="img/24.png" class="lazy lazy-fadeIn"/></a></div>
+		</div><br/>
+		<div class="row">
+			<div class="col-auto"><a href="25.html"><img src="img/25.jpg" class="lazy lazy-fadeIn"/></a></div>
+			<div class="col-auto"><a href="26.html"><img src="img/26.jpg" class="lazy lazy-fadeIn"/></a></div>
+			<div class="col-auto"><a href="27.html"><img src="img/27.jpg" class="lazy lazy-fadeIn"/></a></div>
+			<div class="col-auto"><a href="28.html"><img src="img/28.jpg" class="lazy lazy-fadeIn"/></a></div>
+		</div><br/>
+		<div class="row">
+			<div class="col-auto"><a href="29.html"><img src="img/29.jpg" class="lazy lazy-fadeIn"/></a></div>
+			<div class="col-auto"><a href="30.html"><img src="img/30.jpg" class="lazy lazy-fadeIn"/></a></div>
+			<div class="col-auto"><a href="31.html"><img src="img/31.jpg" class="lazy lazy-fadeIn"/></a></div>
+			<div class="col-auto"><a href="32.html"><img src="img/32.jpg" class="lazy lazy-fadeIn"/></a></div>
+		</div><br/>
+		<div class="row">
+			<div class="col-auto"><a href="33.html"><img src="img/33.jpg" class="lazy lazy-fadeIn"/></a></div>
+		</div>
+	</div>
 </center>;
 
 ReactDOM.render(dom, document.getElementById('c'));
@@ -82,3 +83,28 @@ ReactDOM.render(dom, document.getElementById('c'));
 //     <GridCol><img src="img/2.jpg"/></GridCol>
 //     <GridCol><img src="img/3.jpg"/></GridCol>
 // </GridRow>
+// <div class="content-block-title">COLUMNS WITH EQUAL WIDTH</div>
+//   <div class="content-block">
+//     <div class="row">
+//       <div class="col-auto">.col-auto</div>
+//       <div class="col-auto">.col-auto</div>
+//     </div>
+//     <div class="row">
+//       <div class="col-auto">.col-auto</div>
+//       <div class="col-auto">.col-auto</div>
+//       <div class="col-auto">.col-auto</div>
+//     </div>
+//     <div class="row">
+//       <div class="col-auto">.col-auto</div>
+//       <div class="col-auto">.col-auto</div>
+//       <div class="col-auto">.col-auto</div>
+//       <div class="col-auto">.col-auto</div>
+//     </div>
+//     <div class="row no-gutter">
+//       <div class="col-auto">.col-auto</div>
+//       <div class="col-auto">.col-auto</div>
+//       <div class="col-auto">.col-auto</div>
+//       <div class="col-auto">.col-auto</div>
+//       <div class="col-auto">.col-auto</div>
+//     </div>
+//   </div>          
